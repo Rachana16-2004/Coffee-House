264d0c6ebb665583398e118e50e33fb8941c7236
Rachana16-2004 <bolarachanabola@gmail.com>
2025-08-25 19:46:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle menu for mobile
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
